961ef91002ced38e3ac4bd5d57c31a5ae5b3f8b3
Darylldd <aash100227@gmail.com>
2025-05-31 20:33:11 +0800

    eeeee

--- app.js
+++ app.js
@@ -91,7 +91,11 @@ app.use("/harvest", harvestRoutes);
 
 
 
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, '0.0.0.0', () => {
+  //console.log(`Server started on http://0.0.0.0:${PORT}`);
+//});
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server started on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
