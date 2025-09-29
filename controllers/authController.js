831d8bc344049294230b9aaaa65e6cb19c2a2549
Darylldd <aash100227@gmail.com>
2025-09-29 20:39:08 +0800

    help!

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const auditController = require('./auditController');
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 
-// Configure Sendinblue API
+// Configure Sendinblue APII
+
 const client = SibApiV3Sdk.ApiClient.instance;
 client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
